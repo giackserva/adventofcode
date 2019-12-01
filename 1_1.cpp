10bdde053259bc4d5f170791776d33a5c1a9b190
giackserva <giackserva@gmail.com>
2019-12-01 11:25:37 +0000

    1_1

---
+++ 1_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int sum = 0;
+    for (std::string s; std::getline(std::cin, s); ) {
+        sum += std::stoi(s) / 3 - 2;
+    }
+    std::cout << sum << '\n';
+}
