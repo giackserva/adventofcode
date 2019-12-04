ef84bc6d7d99e1bf04d15b62324706f9bb5e053e
giackserva <giackserva@gmail.com>
2019-12-04 19:23:26 +0000

    day 4

---
+++ day4/4_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool isValid(int n) {
+    vector<int> occ(10, 0);
+    int prev = 10;
+    while(n > 0) {
+        const int curr = n % 10;
+        if(curr > prev) return false;
+        occ[curr]++;
+        n /= 10;
+        prev = curr;
+    }
+    return any_of(occ.begin(), occ.end(), [](int i){ return i > 1; });
+}
+
+int main() {
+    int count = 0;
+    for (int i = 248345; i <= 746315; i++)
+        count += isValid(i);
+    cout << count << '\n';
+}
